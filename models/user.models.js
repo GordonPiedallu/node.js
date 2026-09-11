69eb4a89aea0ab9e167a11e2ced21772bac991c8
Gordon PIEDALLU <gordonpiedallu@gmail.com>
2026-09-11 14:51:09 +0200

    Modification du role user par client

--- models/user.models.js
+++ models/user.models.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
-const { authenticate, isAdmin } = require("../middlewares/auth.middlewares");
-const userSchema = new mongoose.Schema({
 
+const userSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true, minlength: 5, maxlength: 30, trim: true },
     email: { type: String, required: true, unique: true, lowercase: true },
-    password: { type: String, required: true, minlength: 6, minUppercase: 1, minNumbers: 1, minSymbols: 1 },
-    role: { type: String, enum: ["client", "préparateur", "admin"], default: "client" },
+    password: { type: String, required: true, minlength: 6 },
+    role: { type: String, enum: ["client", "admin", "préparateur"], default: "client" },
     bio: { type: String },
-    avatar: { type: String },
-
+    avatar: { type: String }
 }, { timestamps: true });
 
 const User = mongoose.model("User", userSchema);
